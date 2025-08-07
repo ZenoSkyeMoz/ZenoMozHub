ddbf5c10243257e84bb05e89cb6c04b5d3ca9f4f
ZenoSkyeMoz <CODMSHEESHROBLOX@gmail.com>
2025-08-07 19:06:43 +0800

    Update stars.cs

--- stars.cs
+++ stars.cs
@@ -6,34 +6,42 @@ canvas.height = window.innerHeight;
 
 let stars = [];
 
-for (let i = 0; i < 150; i++) {
+for (let i = 0; i < 200; i++) {
   stars.push({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
     radius: Math.random() * 1.5,
-    speed: Math.random() * 0.5 + 0.2
+    speed: Math.random() * 0.6 + 0.2
   });
 }
 
 function drawStars() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#ffffff";
+  ctx.fillStyle = "white";
   ctx.shadowBlur = 5;
-  ctx.shadowColor = "#ffffff";
+  ctx.shadowColor = "white";
 
-  for (let i = 0; i < stars.length; i++) {
-    const s = stars[i];
+  stars.forEach(star => {
     ctx.beginPath();
-    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+    ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
     ctx.fill();
 
-    s.y += s.speed;
-    if (s.y > canvas.height) {
-      s.y = 0;
-      s.x = Math.random() * canvas.width;
+    star.y += star.speed;
+    if (star.y > canvas.height) {
+      star.y = 0;
+      star.x = Math.random() * canvas.width;
     }
-  }
+  });
 
+  requestAnimationFrame(drawStars);
+}
+
+drawStars();
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
   requestAnimationFrame(drawStars);
 }
 
